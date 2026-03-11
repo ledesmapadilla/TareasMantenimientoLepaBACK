6edc99e1792a2f42659ae490ee2b43544750e645
IgnacioLP <ignaciolp@lepa.com.ar>
2026-03-11 16:24:05 -0300

    fix: rename dbconfig.js to dbConfig.js for case-sensitive systems

--- src/server/dbConfig.js
+++ src/server/dbConfig.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 import "colors";
 
-try {
-  mongoose.connect(process.env.MONGODB).then(() => {
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
     console.info(
       `Base de datos ${mongoose.connection.name.green} conectada exitosamente`,
     );
+  })
+  .catch((error) => {
+    console.error("Error al conectar a MongoDB:", error);
   });
-} catch (error) {
-  console.error(error);
-}
 
 export default mongoose;
